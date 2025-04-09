d008c10385173305ae41bbfbdab06d41900517fc
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-10 01:08:34 +0530

    Update saveCookie.js

--- backend/utils/saveCookie.js
+++ backend/utils/saveCookie.js
@@ -3,6 +3,6 @@ export default function saveCookie(token, res) {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         sameSite: "strict",
-        secure: process.env.NODE_ENV !== 'development'
+        secure: true'
     })
 }
