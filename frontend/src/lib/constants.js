48b72b76350e3a8a2fe394cfbbcda655ede99928
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-10 01:02:17 +0530

    Update constants.js

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.MODE === "development" ? import.meta.env.VITE_SERVER_URL : "/"
+export const HOST = import.meta.env.VITE_SERVER_URL
 export const PRODUCTS = 'api/products'
 export const GET_USER_INFO = 'api/auth/user/get-user-info'
 export const LOGIN_ROUTE = `api/auth/user/login`
