1361046088b716139813c025a44873366c14044d
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-10 09:06:42 +0530

    Update constants.js

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,4 @@
-export const HOST = import.meta.env.VITE_SERVER_URL
+export const HOST = 'https://feedback-app-backend-lxun.onrender.com'
 export const PRODUCTS = '/api/products'
 export const GET_USER_INFO = '/api/auth/user/get-user-info'
 export const LOGIN_ROUTE = `/api/auth/user/login`
