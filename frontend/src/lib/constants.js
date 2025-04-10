bb26c5ac09bb5fb35eaef08072497d760a7e345f
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-04-10 09:16:53 +0530

    Update constants.js

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,7 +1,7 @@
-export const HOST = 'https://feedback-app-backend-lxun.onrender.com'
-export const PRODUCTS = '/api/products'
-export const GET_USER_INFO = '/api/auth/user/get-user-info'
-export const LOGIN_ROUTE = `/api/auth/user/login`
-export const LOGOUT_ROUTE = `/api/auth/user/logout`
-export const REGISTER_ROUTE = `/api/auth/user/register`
-export const ADD_FEEDBACK = '/api/feedback'
+export const HOST = import.meta.env.VITE_SERVER_URL
+export const PRODUCTS = 'api/products'
+export const GET_USER_INFO = 'api/auth/user/get-user-info'
+export const LOGIN_ROUTE = `api/auth/user/login`
+export const LOGOUT_ROUTE = `api/auth/user/logout`
+export const REGISTER_ROUTE = `api/auth/user/register`
+export const ADD_FEEDBACK = 'api/feedback'
